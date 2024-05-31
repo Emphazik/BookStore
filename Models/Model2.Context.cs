c62e7533d9717af3dbac95974aa421b893732c0d
Vladislav <vladgutsulyak216@gmail.com>
2024-05-31 11:48:57 +0300

    Добавление бдшки, визуализация вьюшки.

--- Models/Model2.Context.cs
+++ Models/Model2.Context.cs
@@ -27,5 +27,9 @@ namespace BookStore.Models
     
         public virtual DbSet<Roles> Roles { get; set; }
         public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<Authors> Authors { get; set; }
+        public virtual DbSet<Books> Books { get; set; }
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Publishers> Publishers { get; set; }
     }
 }
